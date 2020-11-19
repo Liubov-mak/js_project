8c0106fb613153c7f022dd14847cc4e420c9a46e
Любовь Макарова <arsenii07@gmail.com>
2020-11-19 15:28:09 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const money = 1;
+alert(money);
+
+const income = 'Привет, мир!';
+console.log(income);
+
+const addExpenses = document.querySelector('.addExpenses');
+const deposit = document.querySelector('.deposit');
+const mission = document.querySelector('.mission');
+const period = document.querySelector('.period');
+
