0ecf0a50911767899f0ad385aa56c92adb84868a
Любовь Макарова <arsenii07@gmail.com>
2020-11-21 15:41:26 +0300

    Усложненное задание

--- js/main.js
+++ js/main.js
@@ -1,41 +1,10 @@
 'use strict';
 
-/* const money = 1;
-alert(money);
-
-const income = 'Привет, мир!';
-console.log(income);
-
-const addExpenses = document.querySelector('.addExpenses');
-const deposit = document.querySelector('.deposit');
-const mission = document.querySelector('.mission');
-const period = document.querySelector('.period'); */
-
-let money = 550;
-console.log(typeof money);
-
-let income = 'фриланс';
-console.log(typeof income);
-
-let addExpenses = 'Интернет, Такси, Коммуналка';
-console.log(addExpenses.length);
-
-addExpenses = addExpenses.toLowerCase();
-addExpenses = addExpenses.split(' ');
-console.log(addExpenses);
-
-
-let deposit = true;
-console.log(typeof deposit);
-
-let mission = 875650;
-console.log('Цель заработать ' + (mission) + ' рублей/долларов/гривен/юани');
-
-let period = 6;
-console.log('Период равен ' + period + ' месяцев');
-
-let budgetDay;
-budgetDay = money / 30;
-console.log(budgetDay);
-
-
+let num;
+num = 266219;
+num = num+'';
+num = + num[0] * + num[1] * + num[2] * + num[3] * + num[4] * + num[5];
+num = num**3;
+console.log(num);
+num = num+'';
+alert(num.substring(0, 2));
