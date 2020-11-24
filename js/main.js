f6421830821d56974e9baee58887eb4c66c6538f
Любовь Макарова <arsenii07@gmail.com>
2020-11-24 14:20:39 +0300

    lesson04-01

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 'use strict';
 
-/* const money = 1;
-alert(money);
-
-const income = 'Привет, мир!';
-console.log(income);
-
-const addExpenses = document.querySelector('.addExpenses');
-const deposit = document.querySelector('.deposit');
-const mission = document.querySelector('.mission');
-const period = document.querySelector('.period'); */
-
 let money = 550;
 console.log(typeof money);
 
@@ -29,7 +18,7 @@ let deposit = true;
 console.log(typeof deposit);
 
 let mission = 8758;
-console.log('Цель заработать ' + (mission) + ' рублей/долларов/гривен/юани');
+console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`); //!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 let period = 6;
 console.log('Период равен ' + period + ' месяцев');
@@ -37,6 +26,13 @@ console.log('Период равен ' + period + ' месяцев');
 let budgetDay;
 budgetDay = money / 30;
 console.log(budgetDay);
+
+let showTypeOf = function(data) {
+    console.log(data, typeof(data));
+};
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
 //lesson03
 
@@ -54,22 +50,21 @@ let expenses2;
 let amount1;
 let amount2;
 
-if (expenses1 === true) {
-    amount1 = amount1;    
-} else {
-    amount1 = 0;
-}
-
-
-
-expenses1 = confirm('Введите обязательную статью расходов?');
+/* expenses1 = confirm('Введите обязательную статью расходов?');
 console.log(expenses1);
 amount1 = prompt('Во сколько это обойдется?');
 console.log(amount1);
 expenses2 = confirm('Введите обязательную статью расходов?');
 console.log(expenses2);
 amount2 = prompt('Во сколько это обойдется?');
-console.log(amount2);
+console.log(amount2); */
+
+if (amount1 === null) {
+    amount1 = 0;
+}
+if (amount2 === null) {
+    amount2 = 0;
+}
 
 let budgetMonth;
 budgetMonth = budgetDay * 30 + parseFloat(amount1) + parseFloat(amount2);
@@ -83,8 +78,6 @@ budgetDay = budgetMonth / 30;
 budgetDay = parseFloat(Math.floor(budgetDay));
 console.log(budgetDay);  
 
-budgetDay = 800;
-
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
 } else if (budgetDay > 600 && budgetDay < 1200) {
@@ -95,24 +88,14 @@ if (budgetDay >= 1200) {
     console.log('Что то пошло не так');
 }
 
-
-/* let item = 300;
-
-if (item >= 500) {
-    console.log('Супер');
-} else if (item > 300 && item < 500) {
-    console.log('Среднячек');
-} else if (item >= 0 && item <= 300) {
-    console.log('Лузер');
-} else {
-    console.log('Упс');
-} */
-
 //lesson 03-02
 let namePerson = 'Максим';
 let userName = (namePerson === 'Артем') ? 'Директор' : 
     (namePerson === 'Максим') ? 'Преподаватель' :
     'студент';
     console.log(userName);
+
+//lesson 04-01
+
 
 
