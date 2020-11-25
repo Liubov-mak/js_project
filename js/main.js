a268ca36c150250d0c867831f005fad545ba74f7
Любовь Макарова <arsenii07@gmail.com>
2020-11-25 22:05:52 +0300

    lesson05

--- js/main.js
+++ js/main.js
@@ -1,63 +1,44 @@
 'use strict';
 
-let money = 550;
-console.log(typeof money);
+/* let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}; */
 
+let money = 500; 
 let income = /* 'фриланс' */ 800;
-console.log(typeof income);
-
-let addExpenses = 'Интернет, Такси, Коммуналка';
-console.log(addExpenses.length);
-
-addExpenses = addExpenses.toLowerCase();
-addExpenses = addExpenses.split(' ');
-console.log(addExpenses);
-
-
-let deposit = true;
-console.log(typeof deposit);
-
+let addExpenses = prompt('Перечислите возможные расходы через запятую?');
+console.log(addExpenses.toLowerCase().split(','));
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 8758;
-console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`); //!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+/* console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);  *///!!!!!!!!!!!!!!!!!!!!!!!!!!
 let period = 6;
-console.log('Период равен ' + period + ' месяцев');
-
+/* console.log('Период равен ' + period + ' месяцев'); */
 let budgetDay;
 budgetDay = money / 30;
-console.log(budgetDay);
 
-let showTypeOf = function(data) {
+//
+let start = function() {
+    money = +prompt('Ваш месячный доход?');
+
+    while (isNaN(parseFloat(money))) {
+        money = +prompt('Ваш месячный доход?');
+    }
+};
+
+start();
+
+//
+const showTypeOf = function(data) {
     console.log(data, typeof(data));
 };
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-//lesson03
-
-/* money = prompt('Ваш месячный доход?');
-console.log(money);
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpenses);
-
-deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(deposit); */
-
 let expenses1;
 let expenses2;
 let amount1;
 let amount2;
-
-expenses1 = confirm('Введите обязательную статью расходов?');
-console.log(expenses1);
-amount1 = +prompt('Во сколько это обойдется?');
-console.log(amount1);
-expenses2 = confirm('Введите обязательную статью расходов?');
-console.log(expenses2);
-amount2 = +prompt('Во сколько это обойдется?');
-console.log(amount2);
 
 if (amount1 === null) {
     amount1 = 0;
@@ -68,17 +49,17 @@ if (amount2 === null) {
 
 /* let budgetMonth;
 budgetMonth = budgetDay * 30 + amount1 + amount2;
-console.log(budgetMonth); */
+console.log(budgetMonth);
 
-/* let missionTarget = mission / budgetMonth;
+let missionTarget = mission / budgetMonth;
 missionTarget = parseFloat(Math.ceil(missionTarget));
-console.log(missionTarget); */
+console.log(missionTarget);
 
-/* budgetDay = budgetMonth / 30;
+budgetDay = budgetMonth / 30;
 budgetDay = parseFloat(Math.floor(budgetDay));
-console.log(budgetDay);  */
+console.log(budgetDay); */
 
-let getStatusIncome = function() {
+/* let getStatusIncome = function() {
     if (budgetDay >= 1200) {
         return('У вас высокий уровень дохода');
     } else if (budgetDay > 600 && budgetDay < 1200) {
@@ -89,42 +70,58 @@ let getStatusIncome = function() {
         return('Что то пошло не так');
     }
 };
-console.log(getStatusIncome());
-/* getStatusIncome(); */
+console.log(getStatusIncome()); */
 
 //lesson 03-02
-let namePerson = 'Максим';
+/* let namePerson = 'Максим';
 let userName = (namePerson === 'Артем') ? 'Директор' : 
     (namePerson === 'Максим') ? 'Преподаватель' :
     'студент';
-    console.log(userName);
+    console.log(userName); */
 
-//lesson 04-01
+//lesson 04-01 - 05-видео
 
 let getExpensesMonth = function() {
-    return amount1 + amount2 + money;
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            expenses1 = prompt('Введите обязательную статью расходов?', 'Коммуналка');
+        } else if (i === 1) {
+            expenses2 = prompt('Введите обязательную статью расходов?', 'Школа');
+        }
+
+        sum += +prompt('Во сколько это обойдется?');
+    }
+    console.log(sum);
+    return sum;
+    /* return amount1 + amount2; */
 };
-console.log(getExpensesMonth());
+
+let expensesAmount = getExpensesMonth();
+console.log('Расходы за месяц: ' + expensesAmount);
 
 
-
+//
 let getAccumulatedMonth = function() {
-    return income - getExpensesMonth();
+    return income - expensesAmount;
 };
 let accumulatedMonth = getAccumulatedMonth();
 console.log(accumulatedMonth);
 
+//
 let getTargetMonth = function() {
     return mission / accumulatedMonth;
 };
-console.log(getTargetMonth());
+console.log('Цель достигну за: ' + Math.ceil(getTargetMonth()) + ' месяцев');
 
+//
 budgetDay = accumulatedMonth / 30;
-console.log(budgetDay);
+console.log(Math.ceil(budgetDay));
 
 //lesson04-02
 
-const myFunc = function(a, callback) {
+/* const myFunc = function(a, callback) {
     if (typeof a === 'string') {
         callback(a);
     } else {
@@ -135,4 +132,6 @@ myFunc('  ghbистфиырcncbmndbksdbfakjbfakjbfakjbfkawjkwjudkawubfhkbfhk', f
     let b = a.trim().slice(0, 30);
     a = b + '...';
     console.log(a);        
-});
+}); */
+
+//lesson05-01  - нифига не получился!
